e16dc93c432f73814d10423ab00e6184dbbb244b
이원형 <67450169+whl0526@users.noreply.github.com>
2023-06-29 16:18:07 +0900

    Json Serializable

---
+++ lib/ch15_1_2_serializable/test.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_lab/ch13_4_scaffold/test.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'test.g.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+@JsonSerializable()
+class Location {
+  String latitude;
+  String longitude;
+  Location(this.latitude, this.longitude);
+  factory Location.fromJson(Map<String, dynamic> json) => _$LocationFromJson(json);
+  Map<String,dynamic> toJson() => _$LocationToJson(this);
+
+}
+@JsonSerializable(explicitToJson: true)
+class Todo{
+  @JsonKey(name: "id")
+  int todoId;
+  String title;
+  bool completed;
+  Location location;
+
+  Todo(this.todoId, this.title, this.completed, this.location);
+  factory Todo.fromJson(Map<String, dynamic> json) => _$TodoFromJson(json);
+  Map<String,dynamic> toJson() => _$TodoToJson(this);
+
+}
+
+class MyAppState extends State<MyApp> {
+  String jsonStr = '{"id":1, "title":"Hello","completed":false, "location":{"latitude":"37.5","longitude":"127.1"}}';
+  Todo? todo;
+  String result ='';
+
+  onPressDecode(){
+    Map<String,dynamic> map = jsonDecode(jsonStr);
+    todo = Todo.fromJson(map);
+    setState(() {
+      result = "decode : ${todo?.toJson()}";
+    });
+  }
+  onPressEncode(){
+    setState(() {
+      result = "encode : ${jsonEncode(todo)}";
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('test'),),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('$result'),
+              ElevatedButton(
+                onPressed: onPressDecode,
+                child: Text('Decode'),
+              ),
+              ElevatedButton(
+                onPressed: onPressEncode,
+                child: Text('Encode'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
